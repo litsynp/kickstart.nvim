3ddc1e290029d9f94cead389f7dfe4cac333fd64
litsynp <nocte55is@gmail.com>
2023-10-07 15:54:12 +0900

    feat: add more keymaps to neo-tree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,6 +1,13 @@
 -- Unless you are still migrating, remove the deprecated commands from v1.x
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
+vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
+vim.keymap.set("n", "<leader>nt", "<Cmd>Neotree toggle current reveal_force_cwd<CR>")
+vim.keymap.set("n", "<leader>nr", "<Cmd>Neotree reveal<CR>")
+vim.keymap.set("n", "<leader>nc", "<Cmd>:Neotree float reveal_file=<cfile> reveal_force_cwd<CR>")
+vim.keymap.set("n", "<leader>nb", "<Cmd>:Neotree toggle show buffers right<CR>")
+vim.keymap.set("n", "<leader>ngs", "<Cmd>:Neotree float git_status<CR>")
+
 return {
   "nvim-neo-tree/neo-tree.nvim",
   version = "*",
